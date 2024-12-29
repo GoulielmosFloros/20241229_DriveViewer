e227911f64ecd43d17ab178b637bb66a1851e047
Goulielmos Floros <goulielmos13@hotmail.com>
2024-12-29 14:10:56 +0000

    app

--- src/bim-components/DriveViewer/index.ts
+++ src/bim-components/DriveViewer/index.ts
@@ -1,5 +1,6 @@
 import * as OBC from "@thatopen/components";
 
+// Interfaces to keep things consistent
 export interface AuthData {
   clientId: string;
   clientSecret: string;
@@ -17,6 +18,7 @@ export class DriveViewer extends OBC.Component {
   enabled = false;
   static readonly uuid = "49ef0b61-ba7b-4b69-a428-4284d89e7b50";
   private authData: AuthData;
+  // These below will be imported from the .env file
   private CLIENT_ID: string;
   private CLIENT_SECRET: string;
   readonly ENV: string;
@@ -26,14 +28,21 @@ export class DriveViewer extends OBC.Component {
     super(components);
     components.add(DriveViewer.uuid, this);
 
+    // These must match the scopes we added in the Cloud Console
+
     this.scopes = [
       "https://www.googleapis.com/auth/drive",
       "https://www.googleapis.com/auth/drive.install",
     ];
+
+    // These are already set in the Netlify site
+    // Here create a .env file in the root folder and
+    // VITE_ENV to localhost:8888
     this.CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
     this.ENV = import.meta.env.VITE_ENV;
     this.CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET;
 
+    // This will be an object that will be used in multiple netlify functions
     this.authData = {
       clientId: this.CLIENT_ID,
       clientSecret: this.CLIENT_SECRET,
@@ -42,6 +51,8 @@ export class DriveViewer extends OBC.Component {
     };
   }
 
+  // First step in the process, to tell Google that our app will access Drive data
+  // Look in the next step for the functions implementation
   async authorize() {
     const response = await fetch("/.netlify/functions/googleAuth", {
       method: "post",
@@ -52,20 +63,32 @@ export class DriveViewer extends OBC.Component {
     });
 
     const callback = await response.text();
+    // The callback URI will be returned with double quotes,
+    // we need to get rid of them to set the location
     const fixedCallback = callback.replace(/"/g, "");
 
+    // This will basically redirect us to the new URL
     window.location.href = fixedCallback;
   }
 
+  // This function will be used dinamically
+  // in the main file to know in which step we are
   getQueryParams(): Params {
     const urlParams = new URLSearchParams(window.location.search);
+    // The code will be returned by google after authorizing our app
+    // The state is returned by google after we open a file from the Drive UI Integration
+    // aka Open With option
     const code = urlParams.get("code");
     const state = urlParams.get("state") || "";
     if (state) {
       const stateValues = JSON.parse(decodeURIComponent(state));
+      // The id contains the id of the file to be opened.
       const ids = stateValues?.ids;
       return { authCode: null, fileId: ids };
     }
+
+    // Depending on what parameters are returned by google we will return
+    // them accordingly
 
     if (code) {
       return { authCode: code, fileId: null };
@@ -74,6 +97,7 @@ export class DriveViewer extends OBC.Component {
     return { authCode: null, fileId: null };
   }
 
+  // This function will be executed when the code parameter is returned
   async setCode(authCode: string) {
     if (!this.authData) return;
     this.authData.code = authCode;
@@ -88,14 +112,19 @@ export class DriveViewer extends OBC.Component {
 
     const token = await response.text();
 
+    // The result of the call is a token, which is basically an API Key
+    // This will be needed later, so we have to store it.
     localStorage.setItem("googleToken", token);
   }
 
+  // This function will be called when we use Open With in the Drive.
   async getDriveFile(fileId: string): Promise<string | undefined> {
     try {
       const token = localStorage.getItem("googleToken") || undefined;
 
       const response = await fetch(
+        // In the URL below, the alt=media makes the content of the file
+        // appear in the body of the response.
         `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media`,
         {
           method: "GET",
